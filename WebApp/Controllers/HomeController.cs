ffc8842d6196257775e5e2263e83040968d880de
Chandan Gupta Bhagat <chandan.bhagat@bidhee.com>
2017-05-03 08:11:38 +0545

    initial ccommit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Prerana(string abc)
+        {
+            ViewBag.Title = "Prerana";
+            ViewBag.Message = "This is test message from prerana.";
+            ViewBag.Prerana = "Prerana Bajracharya " +abc;
+            ViewBag.Sriti = "Sriti Prajapati "+abc;
+            return View();
+        }
+
+        public ActionResult Sriti()
+        {
+            return View("About");
+        }
+
+        public ActionResult GetNew()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetNew(Models.NewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/NewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class NewModel
+    {
+        public int id { get; set; }
+        public List<string> str { get; set; }
+        public List<NameClass> NM { get; set; }
+    }
+
+    public class NameClass
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
