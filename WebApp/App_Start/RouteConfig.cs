7a13522b3fbd92161364d377ff831e6d7c7f9d68
Chandan Gupta Bhagat <gchandaniw@gmail.com>
2019-02-09 10:45:36 +0545

    changed the homepage

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace WebApp
+﻿namespace WebApp
 {
     public class RouteConfig
     {
@@ -16,7 +9,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Contact", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
